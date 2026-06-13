ba9123270ca82b084d27d7227a154e6235d0beac
Prakhar Rai <prakhar.raicic@gmail.com>
2026-06-13 12:59:48 +0530

    fix: update API_URL to point to the production server

--- client/config.py
+++ client/config.py
@@ -1,4 +1,6 @@
-import os
+# import os
 
 
-API_URL = os.getenv("API_URL", "http://127.0.0.1:8000")
+# API_URL = os.getenv("API_URL", "http://127.0.0.1:8000")
+
+API_URL = "https://medicalassistant-ijbl.onrender.com/"
